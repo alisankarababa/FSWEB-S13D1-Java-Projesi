2c5ebc6af82d6f642b75fbbf0acb01af54ba88cc
Alişan Karababa <alisankarababa@gmail.com>
2023-11-13 13:09:28 +0300

    add isCatPlaying function and tests

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,11 @@ public class Main {
         System.out.println(hasTeen(9, 99, 19)); // true;
         System.out.println(hasTeen(23, 15, 42)); // true;
         System.out.println(hasTeen(22, 23, 34)); //false;
+
+        System.out.println("isCatPlaying Test");
+        System.out.println(isCatPlaying(true, 10)); //false
+        System.out.println(isCatPlaying(false, 36)); //false
+        System.out.println(isCatPlaying(false, 35)); // true
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int hour) {
@@ -35,5 +40,17 @@ public class Main {
     public static boolean hasTeen(int age1, int age2, int age3) {
 
         return inAgeLimit(age1) || inAgeLimit(age2) || inAgeLimit(age3);
+    }
+
+    public static boolean isCatPlaying(boolean isSummer, int temp) {
+
+        int minTemp = 25;
+        int maxTemp = 35;
+
+        if(isSummer)
+            maxTemp = 45;
+
+
+        return minTemp <= temp && maxTemp >= temp;
     }
 }
