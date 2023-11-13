f36154be094595c282930bf220f92efed9c35048
Alişan Karababa <alisankarababa@gmail.com>
2023-11-13 13:05:39 +0300

    shouldWakeUp function and tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("shouldWakeUp Test");
+        System.out.println(shouldWakeUp (true, 1)); //true
+        System.out.println(shouldWakeUp (false, 2)); //false
+        System.out.println(shouldWakeUp (true, 8)); //false
+        System.out.println(shouldWakeUp (true, -1)); //false
+    }
+    public static boolean shouldWakeUp(boolean isBarking, int hour) {
+
+        if(hour < 0 || hour > 23)
+            return false;
+
+        if(!isBarking)
+            return false;
+
+        return hour < 8 || hour > 20;
     }
 }
