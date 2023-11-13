faa5745d8da2d320df4fa46331e3b63891ed168a
Alişan Karababa <alisankarababa@gmail.com>
2023-11-13 13:07:11 +0300

    add hasTeen function and tests

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         System.out.println("shouldWakeUp Test");
         System.out.println(shouldWakeUp (true, 1)); //true
         System.out.println(shouldWakeUp (false, 2)); //false
         System.out.println(shouldWakeUp (true, 8)); //false
         System.out.println(shouldWakeUp (true, -1)); //false
+
+        System.out.println("hasTeen Test");
+        System.out.println(hasTeen(9, 99, 19)); // true;
+        System.out.println(hasTeen(23, 15, 42)); // true;
+        System.out.println(hasTeen(22, 23, 34)); //false;
     }
+
     public static boolean shouldWakeUp(boolean isBarking, int hour) {
 
         if(hour < 0 || hour > 23)
@@ -16,5 +22,18 @@ public class Main {
             return false;
 
         return hour < 8 || hour > 20;
+    }
+
+
+    public static boolean inAgeLimit(int age) {
+        int ageMax = 19;
+        int ageMin = 13;
+
+        return age >=ageMin && age <= ageMax;
+    }
+
+    public static boolean hasTeen(int age1, int age2, int age3) {
+
+        return inAgeLimit(age1) || inAgeLimit(age2) || inAgeLimit(age3);
     }
 }
